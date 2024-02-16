675996c5b5738970d197682df744525f5c1d82d4
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-02-16 15:48:37 +0800

    signalR_url

--- Back/assets/js/backConnect.js
+++ Back/assets/js/backConnect.js
@@ -1,5 +1,5 @@
 {/* <script src="https://cdn.jsdelivr.net/npm/@microsoft/signalr@8.0.0/dist/cjs/index.min.js"></script> */}
-var connect = new signalR.HubConnectionBuilder().withUrl("https://localhost:7109/chatHub").build();
+var connect = new signalR.HubConnectionBuilder().withUrl(url+"/chatHub").build();
 connect.start().then(()=>{
     connect.invoke("sendNewCase","O20240206001");
 });
