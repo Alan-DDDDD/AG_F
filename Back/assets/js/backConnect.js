5294d4571707a01f08fed09662e489a11a341c98
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-02-16 16:25:01 +0800

    1

--- Back/assets/js/backConnect.js
+++ Back/assets/js/backConnect.js
@@ -1,5 +1,5 @@
 {/* <script src="https://cdn.jsdelivr.net/npm/@microsoft/signalr@8.0.0/dist/cjs/index.min.js"></script> */}
-var connect = new signalR.HubConnectionBuilder().withUrl("/chatHub").withAutomaticReconnect().build();
+var connect = new signalR.HubConnectionBuilder().withUrl("https://a12e-61-230-139-110.ngrok-free.app/chatHub").withAutomaticReconnect().build();
 connect.start().then(()=>{
     connect.invoke("sendNewCase","O20240206001");
 });
