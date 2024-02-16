2aab83fa31fe2c343b9b3dcb38263f28e6b3a238
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-02-16 14:41:09 +0800

    1

---
+++ Back/assets/js/auth.js
@@ -0,0 +1,39 @@
+$(`#select`).on(`click`,function(){
+    getA(
+        "Auth",
+        "Get",
+        `nm=${$(`#q_nm`).val().trim() || ""}&type=${$(`#q_el option:selected`).val() || ""}&agree=${$(`#q_agree option:selected`).val() || ""}`)
+    .then(x=>{
+        if(x){
+            bindT();
+            $(`#qArea`).hide(130);
+        }else{
+            alert(msg);
+        }
+    }).catch(x=>{
+        alert(x);
+    });
+});
+
+function bindT(){
+    let t = $(`#authlist`);
+    t.empty();
+    $.each(datalist,(i,d)=>{
+        let b = d.Agree == "Y";
+        let ac = b ? "danger":"success";
+        let at = b ? "取消":"授權";
+        t.append(`<tr>
+                      <td>${d.type}</td>
+                      <td>${d.nm}</td>
+                      <td>${d.phone}</td>
+                      <td>${d.addr}</td>
+                      <td>${d.Stock.numberFormat(0,".",",")}</td>
+                      <td>
+                        <button 
+                            type="button" class="btn btn-${ac} agree" data-id="${d.lineid}" data-type="${d.typeid}">
+                            ${at}
+                        </button>
+                      </td>
+                    </tr>`);
+    });
+}
