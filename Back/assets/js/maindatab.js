7c86e2779898e6872342703a288ae8924b024d8e
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-02-16 17:09:22 +0800

    url

--- Back/assets/js/maindatab.js
+++ Back/assets/js/maindatab.js
@@ -1,4 +1,4 @@
-var url = "https://a8a3-61-230-139-110.ngrok-free.app";
+var url = "https://f02d-61-230-139-110.ngrok-free.app";
 //var url = "https://localhost:7109";
 var fronturl = "https://alan-ddddd.github.io/AG_F/Back/html";
 var datalist;
