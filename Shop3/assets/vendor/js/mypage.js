21f9da9ad397e3aae5106f46d55795d24fbdb608
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-02-22 00:09:07 +0800

    1

--- Shop3/assets/vendor/js/mypage.js
+++ Shop3/assets/vendor/js/mypage.js
@@ -8,16 +8,11 @@ $(function(){
 function mybar(j){
     var jh = j.offset().top;
     var scrollTop = $(window).scrollTop();
-    if(jh > 0){
-
         if(scrollTop>jh){
             j.css("position","fixed");
             j.css("top","0");
-            j.css("display","flex");
         }else{
             j.css("position","");
             j.css("top","0");
-            j.css("display","none");
         }
-    }
 }
