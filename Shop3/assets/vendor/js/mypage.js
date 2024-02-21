86fa5ac785067950e45d11cee4beca698393df1e
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-02-21 23:44:08 +0800

    1

---
+++ Shop3/assets/vendor/js/mypage.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(window).scroll(function(){
+        mybar($(`#.mybar`));
+    });
+});
+
+function mybar(j){
+    var jh = j.offset().top;
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop>jh){
+        j.css("display","none");
+    }
+}
