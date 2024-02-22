9a78fe983b88bf36c895b9dbd8089a6d1db80e60
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-02-22 11:47:27 +0800

    1

--- Shop3/assets/vendor/js/mypage.js
+++ Shop3/assets/vendor/js/mypage.js
@@ -9,8 +9,15 @@ function mybar(j){
         if(scrollTop>=$(`#bi`).height()){
             j.css("opacity","1");
             $(`.mynav`).css("background-color","#baa88b");
+            j.data("view","Y");
         }else{
             j.css("opacity","0");
             $(`.mynav`).css("background-color","");
+            j.data("view","N");
         }
 }
+$(`.mybar`).on('click','li',function(){
+    if($(this).parent().data("view")=="Y"){
+        alert($(this).html());
+    }
+});
