28ea3f80fdaa67e32522f531199bdde6274930d1
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-02-22 11:05:07 +0800

    1

--- Shop3/assets/vendor/js/mypage.js
+++ Shop3/assets/vendor/js/mypage.js
@@ -8,7 +8,9 @@ function mybar(j){
     var scrollTop = $(window).scrollTop();
         if(scrollTop>=$(`#bi`).height()){
             j.css("opacity","1");
+            $(`.mynav`).css("background-color","#baa88b");
         }else{
             j.css("opacity","0");
+            $(`.mynav`).css("background-color","");
         }
 }
