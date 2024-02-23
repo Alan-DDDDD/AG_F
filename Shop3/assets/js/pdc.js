046ff1cc1877af74a34c7d3fdc6e41b001e847c7
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-02-23 15:17:46 +0800

    1

--- Shop3/assets/js/pdc.js
+++ Shop3/assets/js/pdc.js
@@ -1,15 +1,26 @@
-const numberInput = document.getElementById('numberInput');
-    const incrementButton = document.getElementById('increment');
-    const decrementButton = document.getElementById('decrement');
+// const numberInput = document.getElementById('numberInput');
+//     const incrementButton = document.getElementById('increment');
+//     const decrementButton = document.getElementById('decrement');
 
-    incrementButton.addEventListener('click', () => {
-        if(parseInt(numberInput.value) < 10){
-            numberInput.value = parseInt(numberInput.value) + 1;
-        }
-    });
+//     incrementButton.addEventListener('click', () => {
+//         if(parseInt(numberInput.value) < 10){
+//             numberInput.value = parseInt(numberInput.value) + 1;
+//         }
+//     });
 
-    decrementButton.addEventListener('click', () => {
-        if(parseInt(numberInput.value) > 1){
-            numberInput.value = parseInt(numberInput.value) - 1;
-        }
-    });
+//     decrementButton.addEventListener('click', () => {
+//         if(parseInt(numberInput.value) > 1){
+//             numberInput.value = parseInt(numberInput.value) - 1;
+//         }
+//     });
+
+$(`.pdccount`).on('click','.countbtn',function(){
+    let me = $(this);
+    let a = me.data('action');
+    let v = me.parent().find("input");
+    if(a=="plus"&&+v.val()<10){
+        v.val(+v.val()+1);
+    }else if(a=="minus"&&+v.val()>1){
+        v.val(+v.val()-1);
+    }
+});
