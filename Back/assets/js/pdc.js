50d540ce41d91b2af0cf85d6747ce2bda3a897b3
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-02-29 11:24:55 +0800

    1

--- Back/assets/js/pdc.js
+++ Back/assets/js/pdc.js
@@ -114,11 +114,6 @@ function bindT(){
         let b = d.Agree == "Y";
         let ac = b ? "danger":"success";
         let at = b ? "下架":"上架";
-        let v = ddllist["unit"].filter(x=>x.Dataid == d.Unit)[0];
-        let vv = ""
-        if(v){
-            vv = v.Data
-        }
         t.append(`<tr>
                       <td class="ud" data-id="${d.Pdid}" style="cursor:pointer" data-bs-toggle="modal" data-bs-target="#modalCenter">${d.Pdcnm}</td>
                       <td>${d.Price.numberFormat(0,".",",")}</td>
