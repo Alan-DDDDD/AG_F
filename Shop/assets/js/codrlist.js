a07ccc138501d8e0ab642512d43a2e8f76160811
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-03-11 10:31:54 +0800

    1

--- Shop/assets/js/codrlist.js
+++ Shop/assets/js/codrlist.js
@@ -1,8 +1,10 @@
 $(function(){
-    getA("Order","GetList","s=ING&u="+localStorage.getItem(`LIFF_STORE:2003018925-3RglVyA5:context`).userId)
+    getA("Order"
+        ,"GetList"
+        ,"s=ING&u="+JSON.parse(localStorage.getItem(`LIFF_STORE:2003018925-3RglVyA5:context`)).userId)
     .then(x=>{
         if(x){
-            
+
         }else{
             alert(msg);
         }
