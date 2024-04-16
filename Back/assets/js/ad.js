43eac9c13e5801a58569e83e970e0360a9769e25
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-04-16 10:55:01 +0800

    1

--- Back/assets/js/ad.js
+++ Back/assets/js/ad.js
@@ -1,7 +1,21 @@
 var myedit = null;
 ClassicEditor
 .create(document.querySelector( 
-        '#editor'))
+        '#editor'), {
+            fontColor: {
+                colorPicker: {
+                    // Use 'hex' format for output instead of 'hsl'.
+                    format: 'hex'
+                }
+            },
+            fontBackgroundColor: {
+                // Do not display the color picker.
+                colorPicker: false
+            },
+            toolbar: [
+                'heading', 'bulletedList', 'numberedList', 'fontColor', 'fontBackgroundColor', 'undo', 'redo'
+            ]
+        })
 .then(editor=>{
        console.log(editor);
        myedit = editor;
@@ -9,3 +23,6 @@ ClassicEditor
 .catch(error=>{
        console.error(error);
 });
+$(`#save`).on('click',function(){
+    console.log(myedit.getData());
+})
