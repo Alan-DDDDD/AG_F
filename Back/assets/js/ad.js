29e116fc679430855d5e622cbfdfd839ce4c7140
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-04-16 14:53:49 +0800

    1

--- Back/assets/js/ad.js
+++ Back/assets/js/ad.js
@@ -1,28 +1,8 @@
-var myedit = null;
-ClassicEditor
-.create(document.querySelector( 
-        '#editor'), {
-            fontColor: {
-                colorPicker: {
-                    // Use 'hex' format for output instead of 'hsl'.
-                    format: 'hex'
-                }
-            },
-            fontBackgroundColor: {
-                // Do not display the color picker.
-                colorPicker: false
-            },
-            toolbar: [
-                'heading', 'bulletedList', 'numberedList', 'fontColor', 'fontBackgroundColor', 'undo', 'redo'
-            ]
-        })
-.then(editor=>{
-       console.log(editor);
-       myedit = editor;
-})
-.catch(error=>{
-       console.error(error);
-});
+const quill = new Quill("#editor", {
+    theme: "snow",
+  });
+
 $(`#save`).on('click',function(){
-    console.log(myedit.getData());
-})
+    let data = $(`#editor .ql-editor`).html();
+    console.log(data)
+});
