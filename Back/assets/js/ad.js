37f2e8d00b490e4ec9cab00e41403b098ac49d5c
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2024-04-16 09:18:33 +0800

    1

--- Back/assets/js/ad.js
+++ Back/assets/js/ad.js
@@ -1,9 +1,10 @@
-
+var myedit = null;
 ClassicEditor
 .create(document.querySelector( 
         '#editor'))
 .then(editor=>{
        console.log(editor);
+       myedit = editor;
 })
 .catch(error=>{
        console.error(error);
